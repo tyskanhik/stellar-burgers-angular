88d295ccd6cef5cd0d4b02cd021a129a415c6baf
Arctic Fox <tyskanhik1@mail.ru>
2024-07-14 23:39:34 +0300

    create form profile

--- src/app/user/profile/profile-form/profile-form.component.ts
+++ src/app/user/profile/profile-form/profile-form.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { StoreService } from 'src/app/services/store.service';
 
 @Component({
   selector: 'app-profile-form',
@@ -7,13 +8,37 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./profile-form.component.scss']
 })
 export class ProfileFormComponent {
+  protected isUser: boolean = false;
 
-
-  constructor() {}
+  constructor(private store: StoreService) { }
 
   public formProfile = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
+    name: new FormControl(this.store.getUser().name),
+    email: new FormControl(this.store.getUser().email),
     password: new FormControl('')
   })
+
+  input(e: KeyboardEvent) {
+    if (e.target instanceof HTMLInputElement && e.target.value !== null) {
+
+      if (
+        this.store.getUser().name !== this.formProfile.get(['name'])?.value ||
+        this.store.getUser().email !== this.formProfile.get(['email'])?.value ||
+        this.formProfile.get(['password'])?.value
+      ) {
+        this.isUser = true
+      } else {
+        this.isUser = false
+      }
+    }
+  }
+
+  reset() {
+    this.formProfile.reset({
+      name: this.store.getUser().name,
+      email: this.store.getUser().email,
+      password: ''
+    })
+    this.isUser = false;
+  }
 }
