a27808038ed326e4e3440ba8a03f3c9aa623a1d5
Arctic Fox <tyskanhik1@mail.ru>
2024-07-14 21:14:49 +0300

    create form profile

--- src/app/user/profile/profile-form/profile-form.component.ts
+++ src/app/user/profile/profile-form/profile-form.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-profile-form',
   templateUrl: './profile-form.component.html',
-  styleUrls: ['./profile-form.component.css']
+  styleUrls: ['./profile-form.component.scss']
 })
 export class ProfileFormComponent {
 
+
+  constructor() {}
+
+  public formProfile = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    password: new FormControl('')
+  })
 }
