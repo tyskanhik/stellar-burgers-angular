d583c5ba1a68b1198d920ef651dc4ce4d20db9d9
Arctic Fox <tyskanhik1@mail.ru>
2024-07-15 18:13:36 +0300

    заготовка для оздания пайпа с поиском src картинки ингредиента

--- src/app/user/profile/profile-order/profile-order.component.ts
+++ src/app/user/profile/profile-order/profile-order.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/services/api/api.service';
 import { CookieService } from 'src/app/services/cookie.services';
 import { StoreService } from 'src/app/services/store.service';
+import { Ingredient, Order } from 'src/app/services/types/types';
 
 @Component({
   selector: 'app-profile-order',
@@ -10,9 +11,12 @@ import { StoreService } from 'src/app/services/store.service';
 })
 export class ProfileOrderComponent implements OnInit {
   protected isOrders: boolean = false;
-  protected orders: any[] = []
+  protected orders: Order[] = [];
+  protected ingredients?: Ingredient[];
+  protected updateOrders: any = []
+
   constructor(
-    private _apiServices: ApiService, 
+    private _apiServices: ApiService,
     private _storeservices: StoreService,
     private cookie: CookieService
   ) { }
@@ -21,10 +25,26 @@ export class ProfileOrderComponent implements OnInit {
     this._apiServices.getOrderUser(this.cookie.getCookie('accessToken')).subscribe({
       next: (orders) => {
         this._storeservices.setOrderUser(orders.orders),
-        this.orders = orders.orders,
-        this.isOrders = true
+          this.orders = orders.orders,
+          this.isOrders = true
       },
       error: (error) => console.error('Error:', error)
+    })
+
+    this._apiServices.getIngredients().subscribe({
+      next: (ingredients) => {
+        this.ingredients = ingredients.data;
+      },
+      error: (error) => console.error('Error:', error)
+    })
+  }
+
+  log() {
+    this.orders.map(order => {
+      const ingredients = order.ingredients.map(ing => {
+        return this.ingredients?.find(item => item._id === ing)
+      })
+      this.updateOrders.push({...order, ingredients})
     })
   }
 }
