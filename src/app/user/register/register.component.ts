9e20c7e284e1bc8bf61a247601776d7e6983322f
Arctic Fox <tyskanhik1@mail.ru>
2024-07-16 23:22:12 +0300

    create register formGroup

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+
+  public formRegister = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('',[Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required])
+  })
+
+  public register() {
+    console.log(this.formRegister.value);
+  }
 }
